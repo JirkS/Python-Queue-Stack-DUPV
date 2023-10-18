fdfe8f950090ea6fd415ae9f4d263742b28bd010
Jiří Syrovátko <113086098+JirkS@users.noreply.github.com>
2023-10-18 21:10:19 +0200

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,234 @@
+class Node:
+    def __init__(self, v, p, n):
+        """
+        constructor - declare variables
+        :param v: value of Node
+        :param p: link on previous Node
+        :param n: link on next Node
+        """
+        self.value = v
+        self.previous = p
+        self.next = n
+
+    def set_value(self, v):
+        """
+        Checking and setting param _value
+        :param v: value of Node
+        """
+        if (not isinstance(v, int) and
+                not isinstance(v, float) and
+                not type(v) == bool and
+                not type(v) == str and
+                v is not None):
+            raise Exception("Variable \"value\" can be only boolean, int, float, string or None!")
+        else:
+            self._value = v
+
+    def get_value(self):
+        """
+        Returning value of param value
+        :return: value of node
+        """
+        return self._value
+
+    def set_previous(self, p):
+        """
+        Checking and setting _previous
+        :param p: node
+        """
+        if p is not None and not isinstance(p, Node):
+            raise Exception("Variable \"previous\" can be only None or Node!")
+        else:
+            self._previous = p
+
+    def get_previous(self):
+        """
+        Returning previous node
+        :return: previous node
+        """
+        return self._previous
+
+    def set_next(self, n):
+        """
+        Checking and setting node _next
+        :param n: node
+        """
+        if n is not None and not isinstance(n, Node):
+            raise Exception("Variable \"next\" can be only None or Node!")
+        else:
+            self._next = n
+
+    def get_next(self):
+        """
+        Returning next node
+        :return: next node
+        """
+        return self._next
+
+    value = property(get_value, set_value)
+    previous = property(get_previous, set_previous)
+    next = property(get_next, set_next)
+
+
+class My_Queue:
+    def __init__(self):
+        """
+        constructor - declare variables
+        """
+        self.head = None
+        self.tail = None
+
+    def set_head(self, h):
+        """
+        Checking and setting _head
+        :param h: head node
+        """
+        if h is not None and not isinstance(h, Node):
+            raise Exception("Variable \"head\" can be only None or Node!")
+        else:
+            self._head = h
+
+    def get_head(self):
+        """
+        Returning head node
+        :return: head
+        """
+        return self._head
+
+    def set_tail(self, t):
+        """
+        Checking and setting _tail
+        :param t: tail
+        """
+        if t is not None and not isinstance(t, Node):
+            raise Exception("Variable \"tail\" can be only None or Node!")
+        else:
+            self._tail = t
+
+    def get_tail(self):
+        """
+        Returning node tail
+        :return: node tail
+        """
+        return self._tail
+
+    head = property(get_head, set_head)
+    tail = property(get_tail, set_tail)
+
+    def count(self):
+        """
+        Counting number of nodes in queue
+        :return: number of nodes
+        """
+        num_of_nodes = 0
+        tmp_node = self.head
+        if tmp_node is not None:
+            num_of_nodes += 1
+            while tmp_node.next is not None:
+                tmp_node = tmp_node.next
+                num_of_nodes += 1
+        return num_of_nodes
+
+    def add(self, num):
+        """
+        Adds new node with value of num into queue
+        :param num: value of new node
+        """
+        if self.count() == 0:
+            self.head = Node(num, None, None)
+            self.tail = Node(num, None, None)
+        elif self.count() == 1:
+            self.tail = Node(num, self.head, None)
+            self.head.next = self.tail
+        elif self.count() >= 2:
+            self.tail.next = Node(num, self.tail, None)
+            self.tail = self.tail.next
+
+    def pop(self):
+        """
+        Deletes node from head
+        :return: deleted node
+        """
+        if self.count() == 0:
+            return "Queue is empty, nothing to delete"
+        elif self.count() == 1:
+            tmp = self.head
+            self.head = None
+            self.tail = None
+            return str(tmp.value) + " was deleted"
+        elif self.count() >= 2:
+            tmp = self.head
+            self.head = self.head.next
+            return str(tmp.value) + " was deleted"
+
+    def clear(self):
+        """
+        Deletes all nodes from whole queue (clears it - is empty)
+        """
+        self.head = None
+        self.tail = None
+
+    def popAll(self):
+        """
+        Deletes all nodes from whole queue (clears it - is empty)
+        :return: list of all deleted nodes
+        """
+        list_of_Nodes = []
+        while True:
+            text = self.pop()
+            if text != "Queue is empty, nothing to delete":
+                list_of_Nodes.append(text[0:len(text) - 12])
+            else:
+                break
+        return str(list_of_Nodes) + " was deleted"
+
+    def printf(self):
+        """
+        Returns string of values of nodes
+        :return: all values of all nodes in queue (current state of queue)
+        """
+        text = ""
+        if self.count() != 0:
+            element = self.head
+            while element.next is not None:
+                text += str(element.value) + ", "
+                element = element.next
+            text += str(element.value)
+        else:
+            text += "Linked List is empty."
+        return text
+
+
+try:
+    q = My_Queue()
+    print(q.printf())
+    q.add(4)
+    q.add(8)
+    q.add(6)
+    q.add(38)
+    q.add(39)
+    q.add(95)
+    print(q.printf())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    q.add(1)
+    q.add(93)
+    q.add(47)
+    print(q.printf())
+    q.clear()
+    q.add(13)
+    q.add(85)
+    q.add(63)
+    q.add(44)
+    q.add(671)
+    print(q.printf())
+    print(q.popAll())
+    print(q.printf())
+except Exception as e:
+    print(e)
---
+++ Stack.py
@@ -0,0 +1,187 @@
+class Node:
+    def __init__(self, v, n):
+        """
+        constructor - declare variables
+        :param v: value of Node
+        :param n: link to next Node
+        """
+        self.value = v
+        self.next = n
+
+    def set_value(self, v):
+        """
+        Checking and setting param _value
+        :param v: value of Node
+        """
+        if (not isinstance(v, int) and
+                not isinstance(v, float) and
+                not type(v) == bool and
+                not type(v) == str and
+                v is not None):
+            raise Exception("Variable \"value\" can be only boolean, int, float, string or None!")
+        else:
+            self._value = v
+
+    def get_value(self):
+        """
+        Returning value of param value
+        :return: value of node
+        """
+        return self._value
+
+    def set_next(self, n):
+        """
+        Checking and setting node _next
+        :param n: node
+        """
+        if n is not None and not isinstance(n, Node):
+            raise Exception("Variable \"next\" can be only None or Node!")
+        else:
+            self._next = n
+
+    def get_next(self):
+        """
+        Returning next node
+        :return: next node
+        """
+        return self._next
+
+    value = property(get_value, set_value)
+    next = property(get_next, set_next)
+
+
+class Stack:
+    def __init__(self):
+        """
+        constructor - declare variables
+        """
+        self.top = None
+
+    def set_top(self, t):
+        """
+        Checking and setting node _top
+        :param t: top node
+        """
+        if t is not None and not isinstance(t, Node):
+            raise Exception("Variable \"top\" can be only None or Node!")
+        else:
+            self._top = t
+
+    def get_top(self):
+        """
+        Returning top node
+        :return: top node
+        """
+        return self._top
+
+    top = property(get_top, set_top)
+
+    def count(self):
+        """
+        Counting number of nodes in stack
+        :return: number of nodes
+        """
+        num_of_nodes = 0
+        tmp_node = self.top
+        if tmp_node is not None:
+            num_of_nodes += 1
+            while tmp_node.next is not None:
+                tmp_node = tmp_node.next
+                num_of_nodes += 1
+        return num_of_nodes
+
+    def add(self, num):
+        """
+        Adds new node with value of num into stack (on top)
+        :param num: value of new node
+        """
+        if self.count() == 0:
+            self.top = Node(num, None)
+        else:
+            n = Node(num, self.top)
+            self.top = n
+
+    def pop(self):
+        """
+        Deletes node from top
+        :return: deleted node
+        """
+        if self.count() == 0:
+            return "Stack is empty, nothing to delete"
+        else:
+            n = self.top
+            self.top = self.top.next
+            return str(n.value) + " was deleted"
+
+    def clear(self):
+        """
+        Deletes all nodes from whole stack (clears it - is empty)
+        """
+        self.top = None
+
+    def popAll(self):
+        """
+        Deletes all nodes from whole stack (clears it - is empty)
+        :return: list of all deleted nodes
+        """
+        list_of_Nodes = []
+        while True:
+            text = self.pop()
+            if text != "Stack is empty, nothing to delete":
+                list_of_Nodes.append(text[0:len(text) - 12])
+            else:
+                break
+        return str(list_of_Nodes) + " was deleted"
+
+    def printf(self):
+        """
+        Returns string of values of nodes
+        :return: all values of all nodes in stack (current state of stack)
+        """
+        text = ""
+        if self.count() != 0:
+            element = self.top
+            while element.next is not None:
+                text += str(element.value) + ", "
+                element = element.next
+            text += str(element.value)
+        else:
+            text += "Stack is empty."
+        return text
+
+
+try:
+    s = Stack()
+    s.add(8)
+    s.add(1)
+    s.add(5)
+    s.add(7)
+    s.add(-6)
+    print(s.printf())
+    print(s.pop())
+    print(s.printf())
+    print(s.pop())
+    print(s.printf())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.printf())
+    s.add(1)
+    s.add(-36)
+    s.add(62)
+    s.add(93)
+    print(s.printf())
+    s.clear()
+    print(s.printf())
+    print(s.pop())
+    s.add(13)
+    s.add(85)
+    s.add(63)
+    s.add(44)
+    s.add(671)
+    print(s.printf())
+    print(s.popAll())
+    print(s.printf())
+except Exception as e:
+    print(e)
